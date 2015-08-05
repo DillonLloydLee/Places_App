4ed7e039d4304198e997a8d2200a3ce905aedda7
Dillon Lee & Kyle Pratuch <student@epicodus.com>
2015-08-05 11:32:43 -0700

    Create places class in /src.

--- app/app.php
+++ app/app.php
@@ -1,5 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/places.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_places'])) {
+        $_SESSION['list_of_places'] = array();
+    }
 
     $app = new Silex\Application();
 
---
+++ src/places.php
@@ -0,0 +1,61 @@
+<?php
+
+class Place{
+
+    private $description;
+    private $date;
+    private $souvenir;
+    private $image;
+
+    function __construct($description, $date, $souvenir, $image) {
+        $this->description = $description;
+        $this->date = $date;
+        $this->souvenir = $souvenir;
+        $this->image = $image;
+    }
+
+    function setDescription($new_description) {
+        $this->description = (string) $new_description;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function setDate($new_date) {
+        $this->date = (string) $new_date;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function setSouvenir($new_souvenir) {
+        $this->souvenir = (string) $new_souvenir;
+    }
+
+    function getSouvenir() {
+        return $this->souvenir;
+    }
+
+    function setImage($new_image) {
+        $this->image = $new_image;
+    }
+
+    function getImage() {
+        return $this->image;
+    }
+
+    function save() {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll() {
+        return $_SESSION['list_of_places'];
+    }
+
+    static function deleteAll() {
+        $_SESSION['list_of_places'] = array();
+    }
+
+}
